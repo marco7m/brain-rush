0dd9c805e265e75445849042f10668bd1acfbcdd
Marco Aurélio <marco7.m@gmail.com>
2018-09-10 16:26:07 -0300

    Add slow_down error

--- src/game_window.py
+++ src/game_window.py
@@ -6,7 +6,6 @@ from src.position import Position
 from src.hero import Zombie
 from src.ball import Ball
 import src.const as CONST
-import time
 
 
 _image_library = {}
@@ -18,7 +17,7 @@ class GameWindow:
         #pygame.mixer.init()
 
         # coloca o background
-        background = pygame.image.load("img/retangulo.png")
+        background = pygame.image.load("img/cemiterio.jpg")
 
         game_font = pygame.font.SysFont('Comic Sans MS', 45)
         placar_font = pygame.font.SysFont('Comic Sans MS', 20)
@@ -51,11 +50,8 @@ class GameWindow:
         start_ticks = pygame.time.get_ticks()
         time_left = CONST.GAME_TIME
 
-        count_errors_bug = 0
         mirror = False
-        bug = False
         alreadySaid = [False, False, False, False, False, False] 
-        first = True
         opc0 = opc1 = opc2 = opc3 = 0
 
         time_text_surface = game_font.render(str(time_left), False,(255,255,255))
@@ -73,16 +69,18 @@ class GameWindow:
                 if event.type == pygame.USEREVENT:
                     if time_left > 0:
                         seconds=(pygame.time.get_ticks()-start_ticks)/1000
-                        novo = (pygame.time.get_ticks()-start_ticks)/1000
 
                         # só tem possibilidade de bug depois de 8 segundos de jogo
                         if seconds > 8:
-                            bug_probability = random.randint(0,10)
+                            error_probability = random.randint(0,5)
 
-                            if bug_probability == 1:
-                                bug = True
+                            if error_probability == 1:
+                                hero.slow_down()
+                            if error_probability == 0:
+                                hero.random_movement()
+                                
                             else:
-                                bug = False
+                                pass
                             if (seconds % 5) == 0:
                                 mirror = True
                                 opc0 = random.randint(0,3)
@@ -90,7 +88,7 @@ class GameWindow:
                                 opc2 = random.randint(0,3)
                                 opc3 = random.randint(0,3)
                                 hero.zombie_error()
-                            elif((novo % 4) == 0):
+                            elif (seconds % 4) == 0:
                                 mirror = False
                             #countDown sound     
                         if (time_left == 6):
@@ -98,69 +96,77 @@ class GameWindow:
                                 alreadySaid[i] = False
                         if (time_left == 6 and alreadySaid[time_left-1] == False):
                             countdown = pygame.mixer.Sound('src/sounds/5.wav')
-                            countdown.play()
                             alreadySaid[time_left-1] = True
+                            countdown.play()
                         if (time_left == 5 and alreadySaid[time_left-1] == False) :
                             countdown = pygame.mixer.Sound('src/sounds/4.wav')
-                            countdown.play()
                             alreadySaid[time_left-1] = True
+                            countdown.play()
                         if (time_left == 4 and alreadySaid[time_left-1] == False):
                             countdown = pygame.mixer.Sound('src/sounds/3.wav')
-                            countdown.play()
                             alreadySaid[time_left-1] = True
+                            countdown.play()
                         if (time_left == 3 and alreadySaid[time_left-1] == False):
                             countdown = pygame.mixer.Sound('src/sounds/2.wav')
-                            countdown.play()
                             alreadySaid[time_left-1] = True
+                            countdown.play()
                         if (time_left == 2 and alreadySaid[time_left-1] == False):
                             countdown = pygame.mixer.Sound('src/sounds/1.wav')
-                            countdown.play()
                             alreadySaid[time_left-1] = True
+                            countdown.play()
                         if (time_left == 1 and alreadySaid[time_left-1] == False):
                             countdown = pygame.mixer.Sound('src/sounds/0.wav')
-                            countdown.play()
                             alreadySaid[time_left-1] = True
+                            countdown.play()
                         time_left -= 1
                     else:
                         done = True
             
             # pega as teclas pressionadas
             pressed = pygame.key.get_pressed()
-            #print(mirror)
+            
             if pressed[pygame.K_ESCAPE]:
                 quit(0)
 
-            if bug:
-                if pressed[pygame.K_UP]:
-                    hero.human_error()
-                    count_errors_bug+=1
-                elif pressed[pygame.K_DOWN]:
-                    hero.human_error()
-                    count_errors_bug+=1
-                elif pressed[pygame.K_LEFT]:
-                    hero.human_error()
-                    count_errors_bug+=1
-                elif pressed[pygame.K_RIGHT]:
-                    hero.human_error()
-                    count_errors_bug+=1
-            elif mirror:
-                if pressed[pygame.K_UP]:
-                    hero.wrong_way(opc0)
-                elif pressed[pygame.K_DOWN]:
-                    hero.wrong_way(opc1)
-                elif pressed[pygame.K_LEFT]:
-                    hero.wrong_way(opc2)
-                elif pressed[pygame.K_RIGHT]:
-                    hero.wrong_way(opc3)
+#            if bug:
+#                if pressed[pygame.K_UP]:
+#                    hero.human_error()
+#                elif pressed[pygame.K_DOWN]:
+#                    hero.human_error()
+#                elif pressed[pygame.K_LEFT]:
+#                    hero.human_error()
+#                elif pressed[pygame.K_RIGHT]:
+#                    hero.human_error()
+#            elif mirror:
+#                if pressed[pygame.K_UP]:
+#                    hero.wrong_way(opc0)
+#                elif pressed[pygame.K_DOWN]:
+#                    hero.wrong_way(opc1)
+#                elif pressed[pygame.K_LEFT]:
+#                    hero.wrong_way(opc2)
+#                elif pressed[pygame.K_RIGHT]:
+#                    hero.wrong_way(opc3)
             else:
                 if pressed[pygame.K_UP]:
-                    hero.move_up()
+                    if hero.RANDOM_MOVEMENT:
+                        hero.random_movement()
+                    else:
+                        hero.move_up()
                 elif pressed[pygame.K_DOWN]:
-                    hero.move_down()
+                    if hero.RANDOM_MOVEMENT:
+                        hero.random_movement()
+                    else:
+                        hero.move_down()
                 elif pressed[pygame.K_LEFT]:
-                    hero.move_left()
+                    if hero.RANDOM_MOVEMENT:
+                        hero.random_movement()
+                    else:
+                        hero.move_left()
                 elif pressed[pygame.K_RIGHT]:
-                    hero.move_right()
+                    if hero.RANDOM_MOVEMENT:
+                        hero.random_movement()
+                    else:
+                        hero.move_right()
             # checa se a bola está dentro do heroi
             if hero.has_ball_inside(ball):
                 score += 1
@@ -179,14 +185,12 @@ class GameWindow:
                 time_text_surface = game_font.render(str(time_left), False,(255,255,255))
 
             screen.fill((0, 0, 0))
-            screen.blit(background,(0,0))
+            screen.blit(background,(0,70))
             hero_group.update()
             hero_group.draw(screen)
             screen.blit(get_image('img/cerebro.png'), (ball.pos.x, ball.pos.y))
             screen.blit(score_text_surface,(CONST.DISPLAY_SIZE_X - 250,10))
             screen.blit(time_text_surface,(CONST.DISPLAY_SIZE_X/2 - 0,10))
             screen.blit(placar_text_surface,(CONST.DISPLAY_SIZE_X/2 - 450,30))
-            #if count_errors_bug > 1:
-             #   screen.blit(wtf_text_surface,(CONST.DISPLAY_SIZE_X - 0,0))
             pygame.display.flip()
             clock.tick(60)
--- src/hero.py
+++ src/hero.py
@@ -1,6 +1,7 @@
 # -*- encoding:utf-8 -*-
 import pygame
 import time
+import random
 from src.position import Position
 from src.ball import Ball
 import src.const as CONST
@@ -35,7 +36,15 @@ class Zombie(pygame.sprite.Sprite):
         # ver se é necessário depois essa linha
         self.rect = pygame.Rect(x,y,self.width,self.height)
 
-        self.speed = 6
+        self.speed = CONST.ZOMBIE_SPEED
+
+        # variaveis dos erros
+        self.SLOWED_DOWN = False
+        self.start_slow_down = 0
+        
+        self.RANDOM_MOVEMENT = False
+        self.start_random_movement = 0
+
 
     def has_ball_inside(self, ball):
         if (ball.pos.x > self.pos.x and ball.pos.x < (self.pos.x + self.width)) and (ball.pos.y > self.pos.y and ball.pos.y < (self.pos.y + self.height)):
@@ -58,10 +67,37 @@ class Zombie(pygame.sprite.Sprite):
         if self.pos.y < CONST.DISPLAY_SIZE_Y - self.height:
             self.pos.y += self.speed
 
+    def move_random_direction(self):
+        ran_dir = random.randint(0,3)
+        if ran_dir == 0:
+            self.RANDOM_MOVEMENT = False
+            self.move_left()
+        elif ran_dir == 1:
+            self.RANDOM_MOVEMENT = False
+            self.move_right()
+        elif ran_dir == 2:
+            self.RANDOM_MOVEMENT = False
+            self.move_up()
+        elif ran_dir == 3:
+            self.RANDOM_MOVEMENT = False
+            self.move_down()
+
     def update(self):
         self.rect[0] = self.pos.x
         self.rect[1] = self.pos.y
 
+        # faz ele sair do modo slow_down depois de um tempo
+        if self.SLOWED_DOWN:
+            if pygame.time.get_ticks() - self.start_slow_down > 2000:
+                self.speed = CONST.ZOMBIE_SPEED
+                self.SLOWED_DOWN = False
+
+        # faz ele sair do modo random_movement depois de um tempo
+        if self.RANDOM_MOVEMENT:
+            if pygame.time.get_ticks() - self.start_random_movement > 2000:
+                self.RANDOM_MOVEMENT = False
+
+        # faz os frames se alternarem
         self.delay_time_index += 1
         if self.delay_time_index >= self.DELAY_TIME:
             self.delay_time_index = 0
@@ -71,6 +107,15 @@ class Zombie(pygame.sprite.Sprite):
             if self.index >= len(self.images):
                 self.index = 0
             self.image = self.images[self.index]
+
+    def slow_down(self):
+        self.speed = CONST.ZOMBIE_SLOWER_SPEED
+        self.SLOWED_DOWN = True
+        self.start_slow_down = pygame.time.get_ticks()
+
+    def random_movement(self):
+        self.RANDOM_MOVEMENT = True
+        self.start_random_movement = pygame.time.get_ticks()
 
     def zombie_error(self):
         erro = pygame.mixer.Sound('src/sounds/erro.wav')
