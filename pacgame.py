d61adf9d022c97e1a10bc6efa3e9e1019645cbd5
adamhenriquemp <42943994+adamhenriquemp@users.noreply.github.com>
2018-09-11 09:31:28 -0300

    Teste
    teste

--- pacgame.py
+++ pacgame.py
@@ -8,9 +8,9 @@ from src.end_window import EndWindow
 #pygame.init()
 pygame.mixer.init()
 
-pygame.mixer.music.load('sounds/back1.ogg')
-pygame.mixer.music.play(-1)
-pygame.mixer.music.set_volume(0.8)
+#pygame.mixer.music.load('sounds/back1.ogg')
+#pygame.mixer.music.play(-1)
+#pygame.mixer.music.set_volume(0.8)
 
 game_window = GameWindow()
 end_window = EndWindow()
